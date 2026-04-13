ab120fe966d3e58306500c8a0d0074b50217ac1f
krystian <hi@krystian.io>
2026-04-13 16:58:57 -0400

    Add EOD docs freshness gate

--- tools/end_of_day_routine.sh
+++ tools/end_of_day_routine.sh
@@ -5,22 +5,25 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$ROOT_DIR"
 
 echo "[eod] starting end-of-day routine in: $ROOT_DIR"
-echo "[eod] 1/6 transcript-fix"
+echo "[eod] 1/7 transcript-fix"
 make --no-print-directory transcript-fix
 
-echo "[eod] 2/6 transcript-check"
+echo "[eod] 2/7 transcript-check"
 make --no-print-directory transcript-check
 
-echo "[eod] 3/6 doctor-env"
+echo "[eod] 3/7 eod-docs-check"
+make --no-print-directory eod-docs-check
+
+echo "[eod] 4/7 doctor-env"
 make --no-print-directory doctor-env
 
-echo "[eod] 4/6 lint-docs"
+echo "[eod] 5/7 lint-docs"
 make --no-print-directory lint-docs
 
-echo "[eod] 5/6 test"
+echo "[eod] 6/7 test"
 make --no-print-directory test
 
-echo "[eod] 6/6 stop background tasks"
+echo "[eod] 7/7 stop background tasks"
 make --no-print-directory eod-stop
 
 echo "[eod] done"
