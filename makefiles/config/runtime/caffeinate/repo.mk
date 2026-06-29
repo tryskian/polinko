4e9a5868eeefd0c08d41614d6f48540325710c42
krystian fernando <hi@krystian.io>
2026-06-29 12:04:07 -0400

    chore: tighten repo-scoped caffeinate status (#884)

--- makefiles/config/runtime/caffeinate/repo.mk
+++ makefiles/config/runtime/caffeinate/repo.mk
@@ -1,4 +1,4 @@
 # Repo-managed caffeinate repo activity configuration.
-CAFFEINATE_REPO_SLUG ?= polinko
+CAFFEINATE_REPO_SLUG ?= $(notdir $(CURDIR))
 CAFFEINATE_ACTIVE_WINDOW_SECONDS ?= 1800
 CAFFEINATE_ALLOW_GLOBAL_CLEANUP ?= 0
