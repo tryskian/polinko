af642048a20a3bd84754f300e2cca1a5b5b92be1
krystian <hi@krystian.io>
2026-07-02 00:41:55 -0400

    fix: canonicalize local url helper targets

--- makefiles/runtime/local-urls/viz.mk
+++ makefiles/runtime/local-urls/viz.mk
@@ -1,5 +1,5 @@
 # PASS/FAIL viz local URL print and launch targets.
-.PHONY: viz viz-open open-viz
+.PHONY: viz viz-open
 
 viz: server-daemon
 	@set -eu; \
@@ -14,7 +14,7 @@ ifeq ($(LOCAL_BROWSER_LAUNCH),system)
 	bash "$(LOCAL_URL_LAUNCHER_SCRIPT)" "$$URL"
 endif
 
-viz-open open-viz: server-daemon
+viz-open: server-daemon
 	@set -eu; \
 	URL="$(DEV_VIZ_URL)"; \
 	bash "$(LOCAL_URL_LAUNCHER_SCRIPT)" "$$URL"; \
